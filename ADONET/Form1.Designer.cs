9a662c33c9835b6da5dbbbd8eaa6255754ccd640
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-10-05 17:49:57 -0500

    Agregar archivos de proyecto.

---
+++ ADONET/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ADONET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.dgvPeople = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnAgregrProcedimiento = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPeople)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(127, 81);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Location = new System.Drawing.Point(217, 81);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(83, 23);
+            this.btnDesconectar.TabIndex = 1;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnDesconectar_Click);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(320, 81);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 2;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // dgvPeople
+            // 
+            this.dgvPeople.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPeople.Location = new System.Drawing.Point(127, 242);
+            this.dgvPeople.Name = "dgvPeople";
+            this.dgvPeople.Size = new System.Drawing.Size(363, 109);
+            this.dgvPeople.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Cantidad";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(512, 86);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(35, 13);
+            this.lblCantidad.TabIndex = 5;
+            this.lblCantidad.Text = "label1";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(127, 150);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(242, 150);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 7;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(373, 150);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnAgregrProcedimiento
+            // 
+            this.btnAgregrProcedimiento.Location = new System.Drawing.Point(373, 194);
+            this.btnAgregrProcedimiento.Name = "btnAgregrProcedimiento";
+            this.btnAgregrProcedimiento.Size = new System.Drawing.Size(127, 23);
+            this.btnAgregrProcedimiento.TabIndex = 9;
+            this.btnAgregrProcedimiento.Text = "Agregar Procedimiento";
+            this.btnAgregrProcedimiento.UseVisualStyleBackColor = true;
+            this.btnAgregrProcedimiento.Click += new System.EventHandler(this.btnAgregrProcedimiento_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 363);
+            this.Controls.Add(this.btnAgregrProcedimiento);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvPeople);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPeople)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.DataGridView dgvPeople;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnAgregrProcedimiento;
+    }
+}
+
---
+++ ADONET/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADONET
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection = new SqlConnection("Server=DESKTOP-F970KVM; Database=School; Integrated Security=True");
+                connection.Open();
+                MessageBox.Show("Se abrió una conexión");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+          
+
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            connection.Close();
+            MessageBox.Show("Se cerró una conexión");
+
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Listar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void Listar()
+        {
+            SqlCommand command = new SqlCommand("select * from People", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            dgvPeople.DataSource = dataTable;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand("select count(1) from People", connection);
+                int result = (int) command.ExecuteScalar();
+                lblCantidad.Text =Convert.ToString( result);                                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand(
+                    "insert into People values ('"+ txtNombre.Text +"','"+ txtApellido.Text +"')", connection);
+                command.ExecuteNonQuery();
+                Listar();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnAgregrProcedimiento_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand("usp_InsPeople", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter parameter = new SqlParameter();
+                parameter.SqlDbType = SqlDbType.VarChar;
+                parameter.ParameterName = "@FirstName";
+                parameter.Value = txtNombre.Text;
+
+                SqlParameter parameter2 = new SqlParameter();
+                parameter2.SqlDbType = SqlDbType.VarChar;
+                parameter2.ParameterName = "@LastName";
+                parameter2.Value = txtApellido.Text;
+
+                command.Parameters.Add(parameter);
+                command.Parameters.Add(parameter2);
+
+                command.ExecuteNonQuery();
+                Listar();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
